c3af3464e8eabff5bc20aa9bd884bbfe4f18314b
Santiago Basulto <santiago.basulto@gmail.com>
2017-01-31 10:41:18 -0300

    Update run_app.py

--- run_app.py
+++ run_app.py
@@ -1,10 +1,12 @@
 import os
 
 from app import app
+from config import DATABASE_NAME
 
 
 if __name__ == '__main__':
     app.debug = True
+    app.config['DATABASE_NAME'] = DATABASE_NAME
     host = os.environ.get('IP', '0.0.0.0')
     port = int(os.environ.get('PORT', 8080))
     app.run(host=host, port=port)
